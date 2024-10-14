75af1d118853a1649ffabb94b76e4476abd3fe4e
Ivaylo Svetlinov Lilov <95366964+Ivaylo-Lilov@users.noreply.github.com>
2024-10-14 15:51:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+// Obtenemos el contenedor donde se insertarán los nombres de dominio
+let domainContainer = document.getElementById("domain-list");
+
+pronoun.forEach(p => {
+  adj.forEach(a => {
+    noun.forEach(n => {
+      let domainName = p + a + n + ".com";
+      // Creamos un nuevo elemento HTML para cada dominio
+      let listItem = document.createElement("li");
+      listItem.textContent = domainName;
+      // Añadimos el elemento al contenedor
+      domainContainer.appendChild(listItem);
+    });
+  });
+});
