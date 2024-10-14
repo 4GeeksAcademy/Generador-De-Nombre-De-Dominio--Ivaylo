99c03925909756c78343afb97509dab24a6bb1b9
Ivaylo Svetlinov Lilov <95366964+Ivaylo-Lilov@users.noreply.github.com>
2024-10-14 15:56:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -9,17 +9,15 @@ let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-// Obtenemos el contenedor donde se insertarán los nombres de dominio
+
 let domainContainer = document.getElementById("domain-list");
 
 pronoun.forEach(p => {
   adj.forEach(a => {
     noun.forEach(n => {
       let domainName = p + a + n + ".com";
-      // Creamos un nuevo elemento HTML para cada dominio
       let listItem = document.createElement("li");
       listItem.textContent = domainName;
-      // Añadimos el elemento al contenedor
       domainContainer.appendChild(listItem);
     });
   });
